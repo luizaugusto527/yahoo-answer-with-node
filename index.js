b738e829f9e373271e9ca13b19a1551109aadc45
luizaugustotpires@gmail.com <luizaugustotpires@gmail.com>
2021-12-28 10:27:57 -0300

    make-question-page

--- index.js
+++ index.js
@@ -5,21 +5,12 @@ app.set('view engine','ejs')
 app.use(express.static('public'))
 
 
-app.get("/:nome/:lang", (req,res) => {
-    let nome = req.params.nome
-    let lang = req.params.lang
-    let exibirMsg = true;
-    let produtos = [
-        {nome:'Doritos',preco:3.14},
-        {nome:'coca',preco:5},
-        {nome:'Leite',preco:1.45},
-    ]
-    res.render("index",{
-        nome:nome,
-        lang:lang,
-        msg:exibirMsg,
-        produtos:produtos
-    })
+app.get("/", (req,res) => {
+    res.render("index")
+})
+
+app.get("/perguntar",(req,res)=>{
+    res.render("perguntar")
 })
 
 app.listen(8081, () => {
