9ebea6574600ce3e62a4752c99fdb019f58ed130
luizaugustotpires@gmail.com <luizaugustotpires@gmail.com>
2021-12-31 18:11:05 -0300

    finishing the project

--- index.js
+++ index.js
@@ -44,14 +44,34 @@ app.get('/pergunta/:id',(req,res)=>{
         where:{id:id}
     }).then(pergunta =>{
         if(pergunta != undefined){
-            res.render('pergunta',{
-                pergunta:pergunta
+
+            Resposta.findAll({
+                where:{perguntaId:pergunta.id}
+            }).then(respostas =>{
+                res.render('pergunta',{
+                    pergunta:pergunta,
+                    respostas:respostas
+                })
             })
         }else{
             res.redirect('/')
         }
     })
 })
+
+
+app.post("/responder",(req,res)=>{
+  let corpo = req.body.corpo
+  let perguntaId = req.body.pergunta
+  Resposta.create({
+      corpo:corpo,
+      perguntaId:perguntaId
+  }).then(()=>{
+      res.redirect("/pergunta/" + perguntaId)
+  }) 
+})
+
+
 app.listen(8081, () => {
     console.log("Servidor rodando");
 })
